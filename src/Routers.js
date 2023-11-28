b106e773d88765ebcaa0187ec40e867ee6ea0a0e
Tanvi Naik <naiktanvi30@gmail.com>
2023-11-29 00:55:11 +0530

    Configure React app for deployment to GitHub Pages

--- src/Routers.js
+++ src/Routers.js
@@ -7,7 +7,7 @@ import TaskList from './Components/TaskList'
 
 export default function Routers() {
   return (
-    <BrowserRouter basename='/Taskify'>
+    <BrowserRouter basename={window.location.pathname || ''}>
         <Switch>
             <Route path = "/" exact element = {<TaskList/>} />
             <Route path = "/add-task" exact element = {<AddTask/>} />
